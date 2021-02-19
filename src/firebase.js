cdc76534094adb7cd8e05fae07ea7571ab945627
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-02-19 09:31:04 -0800

    added materialUI, deleted counter folder for redux, and working on header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDKoOvF8xZDKkOvyHk1HqBA3BArl1iX_HM",
+  authDomain: "slack-messanger-app.firebaseapp.com",
+  projectId: "slack-messanger-app",
+  storageBucket: "slack-messanger-app.appspot.com",
+  messagingSenderId: "804852664211",
+  appId: "1:804852664211:web:bcfb9e7dd0ac293ec00a66"
+};
