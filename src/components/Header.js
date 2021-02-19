5f7dcac2f1003e8b4657f09be1db7055547d357d
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-02-19 10:53:51 -0800

    added styled components to header components to build out ui

---
+++ src/components/Header.js
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      {/* header left */}
+      <HeaderLeft>
+        <HeaderAvatar
+        //add onClick to login
+        />
+        <AccessTimeIcon />
+      </HeaderLeft>
+      {/* header search */}
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Search Channel" />
+      </HeaderSearch>
+      {/* header right */}
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  flex: 0.3;
+  align-items: center;
+  margin-left: 20px;
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderRight = styled.div`
+  display: flex;
+  flex: 0.3;
+  align-items: flex-end;
+
+  >.MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
