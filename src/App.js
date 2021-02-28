f970771e93873f1a4b588c6dc74e887ac02c9a42
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-02-28 11:24:05 -0800

    finished functionality for the slack app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Chat from "./components/Chat";
 import { useAuthState, userAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
 import Login from "./components/Login";
+import Spinner from "react-spinkit";
 
 function App() {
   const [user, loading] = useAuthState(auth);
@@ -20,6 +21,7 @@ function App() {
             src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
             alt=""
           />
+          <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
         </AppLoadingContents>
       </AppLoading>
     );
@@ -51,8 +53,26 @@ function App() {
 
 export default App;
 
-const AppLoading = styled.div``;
-const AppLoadingContents = styled.div``;
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+const AppLoadingContents = styled.div`
+  display: flex;
+  text-align: center;
+  padding-bottom: 100px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
 
 const AppBody = styled.div`
   display: flex;
